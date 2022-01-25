fc9a24fcbb937f01448cadb347b70f5dd2ab465f
a-khomitskyi <homitskiy.andriy@gmail.com>
2022-01-25 13:20:33 +0200

    config heroku

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -101,7 +101,7 @@ DATABASES['default'].update(db_from_env)
 
 WHITENOISE_USE_FINDERS = True
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
