998c792cceb8845dda35c8e8e7567e0d4c4f510d
a-khomitskyi <homitskiy.andriy@gmail.com>
2022-01-26 13:56:38 +0200

    config heroku

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASS'),
         'HOST': 'localhost',
-        'PORT': '',
+        'PORT': '5432',
     }
 }
 
