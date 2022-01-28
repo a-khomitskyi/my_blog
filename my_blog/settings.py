2c87e68850b4cea458419d87d9bb032999116176
a-khomitskyi <homitskiy.andriy@gmail.com>
2022-01-28 17:02:58 +0200

    config heroku

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -92,8 +92,8 @@ DATABASES = {
 
 # WHITENOISE_USE_FINDERS = True
 
-# db_from_env = dj_database_url.config(conn_max_age=600)
-# DATABASES['default'].update(db_from_env)
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
